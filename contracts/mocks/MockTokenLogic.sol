b15b979fb2fbd0d8dc6c53ee9bc8e2344d1505cf
Paul Vienhage <pevienhage@gmail.com>
2021-06-01 13:17:49 +0200

    Proxy and Generic Proxy Storage Lib (#1)
    * first mocks of a proxy storage libary
    
    * updates and starting tests
    :
    
    * linting
    
    * more tests
    
    * moar tests
    
    * nvmrc and git step changes
    
    * ci experiment
    
    * lint ci fix
    
    * look inside typechain
    
    * try again
    
    * around again
    
    * Update test/proxyTests.ts
    
    Co-authored-by: Jonny Rhea <5555162+jrhea@users.noreply.github.com>
    
    * remove pointless command
    
    * remove mocks and interfaces from cov testing
    
    * Update contracts/simpleProxy.sol
    
    Co-authored-by: NicholasDotSol <nicholas@element.fi>
    
    * Update contracts/mocks/MockTokenLogic.sol
    
    Co-authored-by: NicholasDotSol <nicholas@element.fi>
    
    Co-authored-by: Jonny Rhea <5555162+jrhea@users.noreply.github.com>
    Co-authored-by: NicholasDotSol <nicholas@element.fi>

---
+++ contracts/mocks/MockTokenLogic.sol
@@ -0,0 +1,79 @@
+pragma solidity ^0.8.0;
+
+import "../libraries/Storage.sol";
+import "./StorageRead.sol";
+
+
+contract MockTokenLogic is ReadAndWriteAnyStorage {
+    // This contract is an implementation target for the proxy so uses the
+    // proxy storage lib for safe portable slotting cross upgrades.
+    using Storage for *;
+
+    // We don't declare them but the following are our state variables
+    // uint256 totalSupply
+    // address owner
+    // mapping(address => balance) balances
+    // Access functions which prevent us from typo-ing the variable name below
+    function _getOwner() internal pure returns (Storage.Address storage) {
+        return Storage.addressPtr("owner");
+    }
+
+    function _getTotalSupply() internal pure returns (Storage.Uint256 storage) {
+        return Storage.uint256Ptr("totalSupply");
+    }
+
+    function _getBalancesMapping()
+        internal
+        pure
+        returns (mapping(address => uint256) storage)
+    {
+        return Storage.mappingAddressToUnit256Ptr("balances");
+    }
+
+    constructor(address _owner) {
+        Storage.Address storage owner = Storage.addressPtr("owner");
+        owner.set(_owner);
+    }
+
+    function transfer(address to, uint256 amount) external {
+        mapping(address => uint256) storage balances = _getBalancesMapping();
+
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+
+    function balanceOf(address who) external view returns (uint256) {
+        mapping(address => uint256) storage balances = _getBalancesMapping();
+        return (balances[who]);
+    }
+
+    function totalSupply() external view returns (uint256) {
+        Storage.Uint256 storage totalSupply = _getTotalSupply();
+        return (totalSupply.load());
+    }
+
+    modifier onlyOwner {
+        Storage.Address storage owner = _getOwner();
+        require(msg.sender == owner.load(), "unauthorized");
+        _;
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner() {
+        Storage.Uint256 storage totalSupply = _getTotalSupply();
+        mapping(address => uint256) storage balances = _getBalancesMapping();
+
+        balances[to] += amount;
+        uint256 localTotalSupply = totalSupply.load();
+        totalSupply.set(localTotalSupply + amount);
+    }
+
+    // A function purely for testing which is a totally unrestricted mint
+    function increaseBalance(address to, uint256 amount) external {
+        Storage.Uint256 storage totalSupply = _getTotalSupply();
+        mapping(address => uint256) storage balances = _getBalancesMapping();
+
+        balances[to] += amount;
+        uint256 localTotalSupply = totalSupply.load();
+        totalSupply.set(localTotalSupply + amount);
+    }
+}
