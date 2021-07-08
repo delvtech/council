65ea90153f4024606fc86f9243aece63c2c4df0a
Paul Vienhage <paul@0x.org>
2021-07-08 12:05:20 +0200

    missing immutable

--- contracts/LockingVault.sol
+++ contracts/LockingVault.sol
@@ -14,7 +14,7 @@ contract LockingVault is IVotingVault {
     // Immutables are in bytecode so don't need special storage treatment
     IERC20 immutable token;
     // A constant which is how far back stale blocks are
-    uint256 staleBlockLag;
+    uint256 immutable staleBlockLag;
 
     constructor(IERC20 _token, uint256 _staleBlockLag) {
         token = _token;
