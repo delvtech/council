c30b84f65d1a365800f02819a9009139140ab057
Paul Vienhage <paul@0x.org>
2021-08-03 17:29:25 +0200

    more tests

---
+++ contracts/mocks/MockDoubleSpender.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+
+// taken from our contracts repo
+
+pragma solidity ^0.8.0;
+
+import "../features/Spender.sol";
+
+// Spends twice in one call
+contract MockDoubleSpender {
+    function doubleSpendSmall(Spender spender, uint256 amount) external {
+        spender.smallSpend(amount, msg.sender);
+        spender.smallSpend(amount, msg.sender);
+    }
+
+    function doubleSpendMedium(Spender spender, uint256 amount) external {
+        spender.mediumSpend(amount, msg.sender);
+        spender.mediumSpend(amount, msg.sender);
+    }
+
+    function doubleSpendLarge(Spender spender, uint256 amount) external {
+        spender.largeSpend(amount, msg.sender);
+        spender.largeSpend(amount, msg.sender);
+    }
+}
--- test/spendingTest.ts
+++ test/spendingTest.ts
@@ -6,16 +6,18 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-wit
 import { BigNumberish, BigNumber } from "ethers";
 import { Spender } from "../typechain/Spender";
 import { MockERC20 } from "../typechain/MockERC20";
+import { MockDoubleSpender } from "../typechain/MockDoubleSpender";
 import { createSnapshot, restoreSnapshot } from "./helpers/snapshots";
 
 const { provider } = waffle;
 
-describe("Locking Vault", function () {
+describe("Spender Feature", function () {
   // We use the history tracker and signers in each test
   let spender: Spender;
   const [wallet] = provider.getWallets();
   let signers: SignerWithAddress[];
   let token: MockERC20;
+  let doubler: MockDoubleSpender;
   const one = ethers.utils.parseEther("1");
   const low = one;
   const mid = one.mul(2);
@@ -44,6 +46,15 @@ describe("Locking Vault", function () {
       mid,
       high
     );
+
+    const doublerDeployer = await ethers.getContractFactory(
+      "MockDoubleSpender",
+      signers[0]
+    );
+    doubler = await doublerDeployer.deploy();
+
+    await token.setBalance(spender.address, one.mul(100));
+    await spender.connect(signers[1]).authorize(doubler.address);
   });
 
   // After we reset our state in the fork
@@ -53,7 +64,6 @@ describe("Locking Vault", function () {
   // Before each we snapshot
   beforeEach(async () => {
     // fund the spending address
-    token.setBalance(spender.address, one.mul(100));
     await createSnapshot(provider);
   });
   // After we reset our state in the fork
@@ -95,5 +105,63 @@ describe("Locking Vault", function () {
   it("High spend can't spend over the limit", async () => {
     const tx = spender.largeSpend(high.add(1), signers[3].address);
     await expect(tx).to.be.revertedWith("Spend Limit Exceeded");
+  });
+
+  it("Can't spend more than the low limit in one block", async () => {
+    const tx = doubler.doubleSpendSmall(spender.address, low.sub(1));
+    await expect(tx).to.be.revertedWith("Spend Limit Exceeded");
+  });
+
+  it("Can't spend more than the mid limit in one block", async () => {
+    const tx = doubler.doubleSpendMedium(spender.address, mid.sub(1));
+    await expect(tx).to.be.revertedWith("Spend Limit Exceeded");
+  });
+
+  it("Can't spend more than the high limit in one block", async () => {
+    const tx = doubler.doubleSpendLarge(spender.address, high.sub(1));
+    await expect(tx).to.be.revertedWith("Spend Limit Exceeded");
+  });
+
+  it("Doesn't allow non owner to set limits", async () => {
+    const tx = spender.setLimits([0, 0, 0]);
+    await expect(tx).to.be.revertedWith("Sender not owner");
+  });
+
+  it("Doesn't allow non authorized to spend low", async () => {
+    const tx = spender
+      .connect(signers[1])
+      .smallSpend(low.sub(1), signers[3].address);
+    await expect(tx).to.be.revertedWith("Sender not Authorized");
+  });
+
+  it("Doesn't allow non authorized to spend mid", async () => {
+    const tx = spender
+      .connect(signers[1])
+      .mediumSpend(low.sub(1), signers[3].address);
+    await expect(tx).to.be.revertedWith("Sender not Authorized");
+  });
+
+  it("Doesn't allow non authorized to spend high", async () => {
+    const tx = spender
+      .connect(signers[1])
+      .largeSpend(low.sub(1), signers[3].address);
+    await expect(tx).to.be.revertedWith("Sender not Authorized");
+  });
+
+  it("doesn't allow non owner to remove tokens", async () => {
+    const tx = spender.removeToken(low.sub(1), signers[3].address);
+    await expect(tx).to.be.revertedWith("Sender not owner");
+  });
+
+  it("Allows owner to set limits", async () => {
+    await spender.connect(signers[1]).setLimits([1, 2, 3]);
+    expect(await spender.smallSpendLimit()).to.be.eq(1);
+    expect(await spender.mediumSpendLimit()).to.be.eq(2);
+    expect(await spender.highSpendLimit()).to.be.eq(3);
+  });
+
+  it("Allows owner to transfer out token", async () => {
+    await spender.connect(signers[1]).removeToken(low, signers[3].address);
+    expect(await token.balanceOf(signers[3].address)).to.be.eq(low);
   });
 });
