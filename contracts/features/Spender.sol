d6c58edd6cf12c812219cade89df4c68e639f84d
Paul Vienhage <pevienhage@gmail.com>
2021-08-05 16:29:16 +0200

    Spender Contract (#17)
    * spending contract
    
    * tests
    
    * more tests

--- contracts/features/Spender.sol
+++ contracts/features/Spender.sol
@@ -1,55 +1,124 @@
-// This contract is currently a scaffold, meaning it is unimplemented and
-// just designed to show the shape of future code. The naming and other
-// conventions are 'soft' and more of suggestions that the implementer
-// has full ability to change. Changes which break interface compatibility though
-// should be double checked with the rest of the team.
-
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.3;
 
+import "../libraries/Authorizable.sol";
+import "../interfaces/IERC20.sol";
+
 // Has governance tokens provided by the treasury, can execute calls to spend them
-// Three separate functions which all spending different amounts
+// Three separate functions which all spending different amounts. We enforce
+// spending limit by block so that no governance proposal can execute a bundle
+// of small spend transactions with low quorum.
 
-contract Spender {
-    // A modifier which enforces that this is only called once per block
-    // by loading a storage var for last call and checking it's not this block
-    // and then setting that var to this block
-    modifier oncePerBlock() {
-        _;
-    }
-
-    // Checks that the caller is the governance contract
-    modifier onlyGovernance() {
-        _;
-    }
-
-    // Each of these should be settable by a function which is only callable
-    // by the timelock.
+contract Spender is Authorizable {
+    // Mapping storing how much is spent in each block
+    // Note - There's minor stability and griefing considerations around tracking the expenditure
+    //        per block for all spend limits. Namely two proposals may try to get executed in the same block and have one
+    //        fail on accident or on purpose. These are for semi-rare non contentious spending so
+    //        we do not consider either a major concern.
+    mapping(uint256 => uint256) public blockExpenditure;
+    // The low, medium and high spending barriers
     uint256 public smallSpendLimit;
     uint256 public mediumSpendLimit;
     uint256 public highSpendLimit;
+    // The immutable token contract
+    IERC20 public immutable token;
 
-    function smallSpend(uint256 amount, uint256 destination)
-        external
-        onlyGovernance
-        oncePerBlock
-    {
-        // Checks that amount < small send limit
+    /// @notice Constructs and sets the permissions plus initial state variables
+    /// @param _owner The contract owner who can change spending limits and authorized addresses
+    /// @param _spender The first address authorized to spend
+    /// @param _token the immutable token this contract has a balance of
+    /// @param _smallSpendLimit The limit on how much spending a small spend proposal can do
+    /// @param _mediumSpendLimit The limit on how much spending a medium spend proposal can do
+    /// @param _highSpendLimit The limit on how much spending a large spend proposal can do
+    constructor(
+        address _owner,
+        address _spender,
+        IERC20 _token,
+        uint256 _smallSpendLimit,
+        uint256 _mediumSpendLimit,
+        uint256 _highSpendLimit
+    ) {
+        // Configure access controls, by authorizing the spender and setting the owner.
+        _authorize(_spender);
+        setOwner(_owner);
+        // Set state and immutable variables
+        token = _token;
+        smallSpendLimit = _smallSpendLimit;
+        mediumSpendLimit = _mediumSpendLimit;
+        highSpendLimit = _highSpendLimit;
     }
 
-    function mediumSpend(uint256 amount, uint256 destination)
+    /// @notice Spends up to the small spend limit
+    /// @param amount the amount to spend
+    /// @param destination the destination to send the token to
+    function smallSpend(uint256 amount, address destination)
         external
-        onlyGovernance
-        oncePerBlock
+        onlyAuthorized
     {
-        // Checks that amount < medium send limit
+        _spend(amount, destination, smallSpendLimit);
     }
 
-    function largeSpend(uint256 amount, uint256 destination)
+    /// @notice Spends up to the medium spend limit
+    /// @param amount the amount to spend
+    /// @param destination the destination to send the token to
+    function mediumSpend(uint256 amount, address destination)
         external
-        onlyGovernance
-        oncePerBlock
+        onlyAuthorized
     {
-        // Checks that amount < high spend limit
+        _spend(amount, destination, mediumSpendLimit);
+    }
+
+    /// @notice Spends up to the large spend limit
+    /// @param amount the amount to spend
+    /// @param destination the destination to send the token to
+    function largeSpend(uint256 amount, address destination)
+        external
+        onlyAuthorized
+    {
+        _spend(amount, destination, highSpendLimit);
+    }
+
+    /// @notice The internal function to handle each of the spend call
+    /// @param amount the amount to spend
+    /// @param destination the destination to send the tokens to
+    /// @param limit the per block spending limit enforced by this call
+    function _spend(
+        uint256 amount,
+        address destination,
+        uint256 limit
+    ) internal {
+        // Check that after processing this we will not have spent more than the block limit
+        uint256 spentThisBlock = blockExpenditure[block.number];
+        require(amount + spentThisBlock <= limit, "Spend Limit Exceeded");
+        // Reentrancy is very unlikely in this context, but we still change state first
+        blockExpenditure[block.number] = amount + spentThisBlock;
+        // Transfer tokens
+        token.transfer(destination, amount);
+    }
+
+    /// @notice Sets the low, medium, and high spend limits, must be called by the timelock
+    /// @param limits [low spend limit, medium spend limit, high spend limit]
+    /// @dev This function always sets all limits, to change only one the previous values
+    ///      of the other two must be provided to this call.
+    function setLimits(uint256[] memory limits) external onlyOwner {
+        // Set the spend limits
+        smallSpendLimit = limits[0];
+        mediumSpendLimit = limits[1];
+        highSpendLimit = limits[2];
+    }
+
+    /// @notice Part of the deprecation process, allows the timelock to move all of the funds
+    ///         out of this contract.
+    /// @param amount The amount of tokens to remove, max uint256 for the full balance
+    /// @param destination the destination to send the tokens to
+    function removeToken(uint256 amount, address destination)
+        external
+        onlyOwner
+    {
+        // If they use max then we just transfer out the balance
+        if (amount == type(uint256).max) {
+            amount = token.balanceOf(address(this));
+        }
+        token.transfer(destination, amount);
     }
 }
---
+++ test/spendingTest.ts
@@ -0,0 +1,167 @@
+import "module-alias/register";
+
+import { expect } from "chai";
+import { ethers, network, waffle } from "hardhat";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+import { BigNumberish, BigNumber } from "ethers";
+import { Spender } from "../typechain/Spender";
+import { MockERC20 } from "../typechain/MockERC20";
+import { MockDoubleSpender } from "../typechain/MockDoubleSpender";
+import { createSnapshot, restoreSnapshot } from "./helpers/snapshots";
+
+const { provider } = waffle;
+
+describe("Spender Feature", function () {
+  // We use the history tracker and signers in each test
+  let spender: Spender;
+  const [wallet] = provider.getWallets();
+  let signers: SignerWithAddress[];
+  let token: MockERC20;
+  let doubler: MockDoubleSpender;
+  const one = ethers.utils.parseEther("1");
+  const low = one;
+  const mid = one.mul(2);
+  const high = one.mul(3);
+
+  before(async () => {
+    // Create a before snapshot
+    await createSnapshot(provider);
+    signers = await ethers.getSigners();
+    // deploy the token;
+    const erc20Deployer = await ethers.getContractFactory(
+      "MockERC20",
+      signers[0]
+    );
+    token = await erc20Deployer.deploy("Ele", "test ele");
+
+    const spenderDeployer = await ethers.getContractFactory(
+      "Spender",
+      signers[0]
+    );
+    spender = await spenderDeployer.deploy(
+      signers[1].address,
+      signers[0].address,
+      token.address,
+      low,
+      mid,
+      high
+    );
+
+    const doublerDeployer = await ethers.getContractFactory(
+      "MockDoubleSpender",
+      signers[0]
+    );
+    doubler = await doublerDeployer.deploy();
+
+    await token.setBalance(spender.address, one.mul(100));
+    await spender.connect(signers[1]).authorize(doubler.address);
+  });
+
+  // After we reset our state in the fork
+  after(async () => {
+    await restoreSnapshot(provider);
+  });
+  // Before each we snapshot
+  beforeEach(async () => {
+    // fund the spending address
+    await createSnapshot(provider);
+  });
+  // After we reset our state in the fork
+  afterEach(async () => {
+    await restoreSnapshot(provider);
+  });
+
+  it("Small spend spend up to the low limit", async () => {
+    // try to spend up to the low limit
+    await spender.smallSpend(low.sub(1), signers[3].address);
+    const balance = await token.balanceOf(signers[3].address);
+    expect(balance).to.be.eq(low.sub(1));
+  });
+
+  it("Small spend can't spend over the limit", async () => {
+    const tx = spender.smallSpend(low.add(1), signers[3].address);
+    await expect(tx).to.be.revertedWith("Spend Limit Exceeded");
+  });
+
+  it("Medium spend spend up to the medium limit", async () => {
+    // try to spend up to the medium limit
+    await spender.mediumSpend(mid.sub(1), signers[3].address);
+    const balance = await token.balanceOf(signers[3].address);
+    expect(balance).to.be.eq(mid.sub(1));
+  });
+
+  it("Mid spend can't spend over the limit", async () => {
+    const tx = spender.mediumSpend(mid.add(1), signers[3].address);
+    await expect(tx).to.be.revertedWith("Spend Limit Exceeded");
+  });
+
+  it("High spend spend up to the high limit", async () => {
+    // try to spend up to the high limit
+    await spender.largeSpend(high.sub(1), signers[3].address);
+    const balance = await token.balanceOf(signers[3].address);
+    expect(balance).to.be.eq(high.sub(1));
+  });
+
+  it("High spend can't spend over the limit", async () => {
+    const tx = spender.largeSpend(high.add(1), signers[3].address);
+    await expect(tx).to.be.revertedWith("Spend Limit Exceeded");
+  });
+
+  it("Can't spend more than the low limit in one block", async () => {
+    const tx = doubler.doubleSpendSmall(spender.address, low.sub(1));
+    await expect(tx).to.be.revertedWith("Spend Limit Exceeded");
+  });
+
+  it("Can't spend more than the mid limit in one block", async () => {
+    const tx = doubler.doubleSpendMedium(spender.address, mid.sub(1));
+    await expect(tx).to.be.revertedWith("Spend Limit Exceeded");
+  });
+
+  it("Can't spend more than the high limit in one block", async () => {
+    const tx = doubler.doubleSpendLarge(spender.address, high.sub(1));
+    await expect(tx).to.be.revertedWith("Spend Limit Exceeded");
+  });
+
+  it("Doesn't allow non owner to set limits", async () => {
+    const tx = spender.setLimits([0, 0, 0]);
+    await expect(tx).to.be.revertedWith("Sender not owner");
+  });
+
+  it("Doesn't allow non authorized to spend low", async () => {
+    const tx = spender
+      .connect(signers[1])
+      .smallSpend(low.sub(1), signers[3].address);
+    await expect(tx).to.be.revertedWith("Sender not Authorized");
+  });
+
+  it("Doesn't allow non authorized to spend mid", async () => {
+    const tx = spender
+      .connect(signers[1])
+      .mediumSpend(low.sub(1), signers[3].address);
+    await expect(tx).to.be.revertedWith("Sender not Authorized");
+  });
+
+  it("Doesn't allow non authorized to spend high", async () => {
+    const tx = spender
+      .connect(signers[1])
+      .largeSpend(low.sub(1), signers[3].address);
+    await expect(tx).to.be.revertedWith("Sender not Authorized");
+  });
+
+  it("doesn't allow non owner to remove tokens", async () => {
+    const tx = spender.removeToken(low.sub(1), signers[3].address);
+    await expect(tx).to.be.revertedWith("Sender not owner");
+  });
+
+  it("Allows owner to set limits", async () => {
+    await spender.connect(signers[1]).setLimits([1, 2, 3]);
+    expect(await spender.smallSpendLimit()).to.be.eq(1);
+    expect(await spender.mediumSpendLimit()).to.be.eq(2);
+    expect(await spender.highSpendLimit()).to.be.eq(3);
+  });
+
+  it("Allows owner to transfer out token", async () => {
+    await spender.connect(signers[1]).removeToken(low, signers[3].address);
+    expect(await token.balanceOf(signers[3].address)).to.be.eq(low);
+  });
+});
