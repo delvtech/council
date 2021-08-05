ebcc0a8a83ab139fdfbd11b11cbab1343842a577
NicholasDotSol <nicholas@element.fi>
2021-08-05 11:45:31 +0300

    Vesting Vault (#18)
    * add time helper
    
    * VestingVaultStorage mod
    
    copied and changed the storage library so we can have
    the Grat struct
    
    * Implement the vestig vault
    
    * Test the vesting vault
    
    * setDefaultQuroum -> setDefaultQuorum
    
    * Add missing comment
    
    * Remove duplicates from storage libraries
    
    * Update contracts/vaults/VestingVault.sol
    
    Co-authored-by: Paul Vienhage <pevienhage@gmail.com>
    
    Co-authored-by: Paul Vienhage <pevienhage@gmail.com>

---
+++ contracts/libraries/VestingVaultStorage.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.3;
+import "./Storage.sol";
+
+// Copy of `Storage` with modified scope to match the VestingVault requirements
+// This library allows for secure storage pointers across proxy implementations
+// It will return storage pointers based on a hashed name and type string.
+library VestingVaultStorage {
+    // This library follows a pattern which if solidity had higher level
+    // type or macro support would condense quite a bit.
+
+    // Each basic type which does not support storage locations is encoded as
+    // a struct of the same name capitalized and has functions 'load' and 'set'
+    // which load the data and set the data respectively.
+
+    // All types will have a function of the form 'typename'Ptr('name') -> storage ptr
+    // which will return a storage version of the type with slot which is the hash of
+    // the variable name and type string. This pointer allows easy state management between
+    // upgrades and overrides the default solidity storage slot system.
+
+    // A struct which represents 1 packed storage location (Grant)
+    struct Grant {
+        uint128 allocation;
+        uint128 withdrawn;
+        uint128 created;
+        uint128 expiration;
+        uint128 cliff;
+        uint128 latestVotingPower;
+        address delegatee;
+    }
+
+    /// @notice Returns the storage pointer for a named mapping of address to uint256[]
+    /// @param name the variable name for the pointer
+    /// @return data the mapping pointer
+    function mappingAddressToGrantPtr(string memory name)
+        internal
+        pure
+        returns (mapping(address => Grant) storage data)
+    {
+        bytes32 typehash = keccak256("mapping(address => Grant)");
+        bytes32 offset = keccak256(abi.encodePacked(typehash, name));
+        assembly {
+            data.slot := offset
+        }
+    }
+}
--- contracts/vaults/VestingVault.sol
+++ contracts/vaults/VestingVault.sol
@@ -1,51 +1,409 @@
-// This contract is currently a scaffold, meaning it is unimplemented and
-// just designed to show the shape of future code. The naming and other
-// conventions are 'soft' and more of suggestions that the implementer
-// has full ability to change. Changes which break interface compatibility though
-// should be double checked with the rest of the team.
-
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.3;
 
-contract VestingVault {
+import "../interfaces/IERC20.sol";
+import "../libraries/History.sol";
+import "../libraries/VestingVaultStorage.sol";
+import "../libraries/Storage.sol";
+import "../interfaces/IVotingVault.sol";
+import "hardhat/console.sol";
+
+contract VestingVault is IVotingVault {
+    // Bring our libraries into scope
+    using History for *;
+    using VestingVaultStorage for *;
+    using Storage for *;
+
+    // NOTE: There is no emergency withdrawal, any funds not sent via deposit() are
+    // unrecoverable by this version of the VestingVault
+
     // This contract has a privileged grant manager who can add grants or remove grants
     // It will not transfer in on each grant but rather check for solvency via state variables.
 
+    // Immutables are in bytecode so don't need special storage treatment
+    IERC20 public immutable token;
+
+    // A constant which is how far back stale blocks are
+    uint256 public immutable staleBlockLag;
+
+    event VoteChange(address indexed to, address indexed from, int256 amount);
+
+    constructor(
+        IERC20 _token,
+        uint256 _stale,
+        address _manager,
+        address _timelock
+    ) {
+        Storage.set(Storage.addressPtr("manager"), _manager);
+        Storage.set(Storage.addressPtr("timelock"), _timelock);
+        Storage.set(Storage.uint256Ptr("unvestedMultiplier"), 100);
+        token = _token;
+        staleBlockLag = _stale;
+    }
+
+    // deposits mapping(address => Grant)
+    /// @notice A single function endpoint for loading grant storage
+    /// @dev Only one Grant is allowed per address. Grants SHOULD NOT
+    /// be modified.
+    /// @return returns a storage mapping which can be used to look up grant data
+    function _grants()
+        internal
+        pure
+        returns (mapping(address => VestingVaultStorage.Grant) storage)
+    {
+        // This call returns a storage mapping with a unique non overwrite-able storage location
+        // which can be persisted through upgrades, even if they change storage layout
+        return (VestingVaultStorage.mappingAddressToGrantPtr("grants"));
+    }
+
+    /// @notice A function to access the storage of the unassigned token value
+    /// @dev The unassigned tokens are not part of any grant and ca be used
+    /// for a future grant or withdrawn by the manager.
+    /// @return A struct containing the unassigned uint.
+    function _unassigned() internal pure returns (Storage.Uint256 storage) {
+        return Storage.uint256Ptr("unassigned");
+    }
+
+    /// @notice A function to access the storage of the manager address.
+    /// @dev The manager can access all functions with the olyManager modifier.
+    /// @return A struct containing the manager address.
+    function _manager() internal pure returns (Storage.Address memory) {
+        return Storage.addressPtr("manager");
+    }
+
+    /// @notice A function to access the storage of the timelock address
+    /// @dev The timelock can access all functions with the onlyTimelock modifier.
+    /// @return A struct containing the timelock address.
+    function _timelock() internal pure returns (Storage.Address memory) {
+        return Storage.addressPtr("timelock");
+    }
+
+    /// @notice A function to access the storage of the unvestedMultiplier value
+    /// @dev The unvested multiplier is a number that represents the voting power of each
+    /// unvested token as a percentage of a vested token. For example if
+    /// unvested tokens have 50% voting power compared to vested ones, this value would be 50.
+    /// This can be changed by governance in the future.
+    /// @return A struct containing the unvestedMultiplier uint.
+    function _unvestedMultiplier()
+        internal
+        pure
+        returns (Storage.Uint256 memory)
+    {
+        return Storage.uint256Ptr("unvestedMultiplier");
+    }
+
     modifier onlyManager() {
         _;
+        require(msg.sender == _manager().data, "!manager");
     }
 
-    // TODO Grant struct type
+    modifier onlyTimelock() {
+        _;
+        require(msg.sender == _timelock().data, "!timelock");
+    }
 
-    function addGrant(
-        address who /*,Grant grant*/
+    /// @notice Getter for the grants mapping
+    /// @param _who The owner of the grant to query
+    /// @return Grant of the provided address
+    function getGrant(address _who)
+        external
+        view
+        returns (VestingVaultStorage.Grant memory)
+    {
+        return _grants()[_who];
+    }
+
+    /// @notice Adds a new grant.
+    /// @dev Manager can set who the voting power will be delegated to initially.
+    /// This potentially avoids the need for a delegation transaction by the grant recipient.
+    /// There is currently no block number sanity check. The cliff can be higher than the expiration.
+    /// @param _who The Grant recipient.
+    /// @param _amount The total grant value.
+    /// @param _expiration timestamp when the grant ends (all tokens count as unlocked).
+    /// @param _cliff Timestamp when the cliff ends. No tokens are unlocked until this
+    /// timestamp is reached.
+    /// @param _delegatee Optional param. The address to delegate the voting power
+    /// associated with this grant to
+    function addGrantAndDelegate(
+        address _who,
+        uint128 _amount,
+        uint128 _expiration,
+        uint128 _cliff,
+        address _delegatee
     ) public onlyManager {
-        // adds the grant for the user, removes the total amount from the stored
-        // balance of this contract
-        // Adds voting power for the holder of the voting vault equal to amount
-        // that they will vest.
+        Storage.Uint256 storage unassigned = _unassigned();
+        Storage.Uint256 memory unvestedMultiplier = _unvestedMultiplier();
+
+        require(unassigned.data >= _amount, "Insufficient balance");
+        // load the grant.
+        VestingVaultStorage.Grant storage grant = _grants()[_who];
+
+        // If this address already has a grant, a different address must be provided
+        // topping up or editing active grants is not supported.
+        require(grant.allocation == 0, "Has Grant");
+
+        // load the delegate. Defaults to the grant owner
+        address _delegatee = _delegatee == address(0) ? _who : _delegatee;
+
+        // calculate the voting power. Assumes all voting power is initially locked.
+        // Come back to this assumption.
+        uint128 newVotingPower =
+            (_amount * uint128(unvestedMultiplier.data)) / 100;
+
+        // set the new grant
+        _grants()[_who] = VestingVaultStorage.Grant(
+            _amount,
+            0,
+            uint128(block.number),
+            _expiration,
+            _cliff,
+            newVotingPower,
+            _delegatee
+        );
+
+        // update the amount of unassigned tokens
+        unassigned.data -= _amount;
+
+        // update the delegatee's voting power
+        History.HistoricalBalances memory votingPower = _votingPower();
+        uint256 delegateeVotes = votingPower.loadTop(grant.delegatee);
+        votingPower.push(grant.delegatee, delegateeVotes + newVotingPower);
+
+        emit VoteChange(grant.delegatee, _who, int256(int128(newVotingPower)));
     }
 
-    function removeGrant(address who) public onlyManager {
-        // Removes the grant for the user and pays out any vested but unclaimed
-        // tokens by sending them to the user. Adds the net about which will
-        // no longer have to be paid back to the stored balance.
+    /// @notice Removes a grant.
+    /// @dev The manager has the power to remove a grant at any time. Any withdrawable tokens will be
+    /// sent to the grant owner.
+    /// @param _who The Grant owner.
+    function removeGrant(address _who) public onlyManager {
+        // load the grant
+        VestingVaultStorage.Grant storage grant = _grants()[_who];
+        // get the amount of withdrawable tokens
+        uint256 withdrawable = _getWithdrawableAmount(grant);
+        // it is simpler to just transfer withdrawable tokens instead of modifying the struct storage
+        // to allow withdrawal through claim()
+        token.transfer(_who, withdrawable);
+
+        Storage.Uint256 storage unassigned = _unassigned();
+        uint256 locked = grant.allocation - (grant.withdrawn + withdrawable);
+
+        // return the unused tokens so they can be used for a different grant
+        unassigned.data += locked;
+
+        // update the delegatee's voting power
+        History.HistoricalBalances memory votingPower = _votingPower();
+        uint256 delegateeVotes = votingPower.loadTop(grant.delegatee);
+        votingPower.push(
+            grant.delegatee,
+            delegateeVotes - grant.latestVotingPower
+        );
+
+        // delete the grant
+        delete _grants()[_who];
+
+        emit VoteChange(
+            grant.delegatee,
+            _who,
+            -1 * int256(int128(grant.latestVotingPower))
+        );
     }
 
-    function deposit(uint256 amount) public onlyManager {
-        // transfers amount from the manager and adds it to solvency
-    }
-
-    function delegate(address to) public {
-        // allows someone who has a vesting grant to delegate their voting power
-    }
-
+    /// @notice Claim all withdrawable value from a grant.
+    /// @dev claiming value resets the voting power, THis could either increase or reduce the
+    /// total voting power associated with the caller's grant.
     function claim() public {
-        // removes all vested tokens for the msg.sender
-        // also removes that voting power
+        // load the grant
+        VestingVaultStorage.Grant storage grant = _grants()[msg.sender];
+        // get the withdrawable amount
+        uint256 withdrawable = _getWithdrawableAmount(grant);
+
+        // transfer the available amount
+        token.transfer(msg.sender, withdrawable);
+        grant.withdrawn += uint128(withdrawable);
+
+        // update the user's voting power
+        _syncVotingPower(msg.sender, grant);
     }
 
-    function queryVotingPower(address who, uint256 when) public {
-        // function which looks up who's voting power at block when, fulfills the vault interface
+    /// @notice Changes the caller's token grant voting power delegation.
+    /// @dev The total voting power is not guaranteed to go up because
+    /// the unvested token multiplier can be updated at any time.
+    function delegate(address _to) public {
+        VestingVaultStorage.Grant storage grant = _grants()[msg.sender];
+        History.HistoricalBalances memory votingPower = _votingPower();
+
+        uint256 oldDelegateeVotes = votingPower.loadTop(grant.delegatee);
+        uint256 newDelegateeVotes = votingPower.loadTop(_to);
+        uint256 newVotingPower = _currentVotingPower(grant);
+
+        // Remove old delegatee's voting power and emit event
+        votingPower.push(
+            grant.delegatee,
+            oldDelegateeVotes - grant.latestVotingPower
+        );
+        emit VoteChange(
+            grant.delegatee,
+            msg.sender,
+            -1 * int256(int128(grant.latestVotingPower))
+        );
+
+        // add voting power to the target delegatee and emit event
+        emit VoteChange(_to, msg.sender, int256(newVotingPower));
+        votingPower.push(_to, newDelegateeVotes + newVotingPower);
+
+        // update grant info
+        grant.latestVotingPower = uint128(newVotingPower);
+        grant.delegatee = _to;
+    }
+
+    /// @notice Manager-only token deposit function.
+    /// @dev Deposited tokens are added to `_unassigned` and can be used to create grants.
+    /// WARNING: This is the only way to deposit tokens into the contract. Any tokens sent
+    /// via other means are not recoverable by this contract.
+    /// @param _amount The amount of tokens to deposit.
+    function deposit(uint256 _amount) public onlyManager {
+        Storage.Uint256 storage unassigned = _unassigned();
+        // update unassigned value
+        unassigned.data += _amount;
+        token.transferFrom(msg.sender, address(this), _amount);
+    }
+
+    /// @notice Manager-only token withdrawal function.
+    /// @dev The manager can withdraw tokens that are not being used by a grant.
+    /// This function cannot be used to recover tokens that were sent to this contract
+    /// by any means other than `deposit()`
+    function withdraw(uint256 _amount, address _recipient) public onlyManager {
+        Storage.Uint256 storage unassigned = _unassigned();
+        require(unassigned.data >= _amount, "Insufficient balance");
+        // update unassigned value
+        unassigned.data -= _amount;
+        token.transfer(_recipient, _amount);
+    }
+
+    /// @notice Update a delegatee's voting power.
+    /// @dev Voting power is only updated for this block onward.
+    /// see `History` for more on how voting power is tracked and queried.
+    /// Anybody can update a grant's voting power.
+    function updateVotingPower(address _who) public {
+        VestingVaultStorage.Grant storage grant = _grants()[_who];
+        _syncVotingPower(_who, grant);
+    }
+
+    /// @notice Helper to update a delegatee's voting power.
+    function _syncVotingPower(
+        address _who,
+        VestingVaultStorage.Grant storage _grant
+    ) internal {
+        History.HistoricalBalances memory votingPower = _votingPower();
+
+        uint256 delegateeVotes = votingPower.loadTop(_grant.delegatee);
+
+        uint256 newVotingPower = _currentVotingPower(_grant);
+        // get the change in voting power. Negative if the voting power is reduced
+        int256 change =
+            int256(newVotingPower) - int256(int128(_grant.latestVotingPower));
+        // do nothing if there is no change
+        if (change == 0) return;
+        if (change > 0) {
+            votingPower.push(
+                _grant.delegatee,
+                delegateeVotes + uint256(change)
+            );
+        } else {
+            // if the change is negative, we multiply by -1 to avoid underflow when casting
+            votingPower.push(
+                _grant.delegatee,
+                delegateeVotes - uint256(change * -1)
+            );
+        }
+        emit VoteChange(_grant.delegatee, _who, change);
+        _grant.latestVotingPower = uint128(newVotingPower);
+    }
+
+    /// @notice Attempts to load the voting power of a user
+    /// @param user The address we want to load the voting power of
+    /// @param blockNumber the block number we want the user's voting power at
+    /// @return the number of votes
+    function queryVotePower(address user, uint256 blockNumber)
+        external
+        override
+        returns (uint256)
+    {
+        // Get our reference to historical data
+        History.HistoricalBalances memory votingPower = _votingPower();
+        // Find the historical data and clear everything more than 'staleBlockLag' into the past
+        return
+            votingPower.findAndClear(
+                user,
+                blockNumber,
+                block.number - staleBlockLag
+            );
+    }
+
+    /// @notice Loads the voting power of a user without changing state
+    /// @param user The address we want to load the voting power of
+    /// @param blockNumber the block number we want the user's voting power at
+    /// @return the number of votes
+    function queryVotePowerView(address user, uint256 blockNumber)
+        external
+        view
+        returns (uint256)
+    {
+        // Get our reference to historical data
+        History.HistoricalBalances memory votingPower = _votingPower();
+        // Find the historical data
+        return votingPower.find(user, blockNumber);
+    }
+
+    function _getWithdrawableAmount(VestingVaultStorage.Grant memory _grant)
+        internal
+        view
+        returns (uint256)
+    {
+        if (block.number < _grant.cliff) {
+            return 0;
+        }
+        if (block.number >= _grant.expiration) {
+            return (_grant.allocation - _grant.withdrawn);
+        }
+        uint256 unlocked =
+            (_grant.allocation * (block.number - _grant.created)) /
+                (_grant.expiration - _grant.created);
+        return (unlocked - _grant.withdrawn);
+    }
+
+    /// @notice Returns the historical voting power tracker.
+    /// @return A struct which can push to and find items in block indexed storage.
+    function _votingPower()
+        internal
+        pure
+        returns (History.HistoricalBalances memory)
+    {
+        // This call returns a storage mapping with a unique non overwrite-able storage location
+        // which can be persisted through upgrades, even if they change storage layout.
+        return (History.load("votingPower"));
+    }
+
+    /// @notice Helper that returns the current voting power of a grant
+    /// @dev This is not always the recorded voting power since it uses the latest
+    /// _unvestedMultiplier.
+    /// @param _grant The grant to check for voting power.
+    /// @return The current voting power of the grant.
+    function _currentVotingPower(VestingVaultStorage.Grant memory _grant)
+        internal
+        view
+        returns (uint256)
+    {
+        uint256 withdrawable = _getWithdrawableAmount(_grant);
+        uint256 locked = _grant.allocation - (withdrawable + _grant.withdrawn);
+        return (withdrawable + (locked * _unvestedMultiplier().data) / 100);
+    }
+
+    /// @notice timelock-only unvestedMultiplier update function.
+    /// @dev Allows the timelock to update the unvestedMultiplier.
+    /// @param _multiplier The new multiplier.
+    function changeUnvestedMultiplier(uint256 _multiplier) public onlyTimelock {
+        Storage.set(Storage.uint256Ptr("unvestedMultiplier"), _multiplier);
     }
 }
