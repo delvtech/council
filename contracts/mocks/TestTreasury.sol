78e3c11acb94ae25567c801b413f3c5a595af7ec
NicholasDotSol <nicholas@element.fi>
2021-08-11 04:24:00 +0300

    Treasury (#21)
    * Implement Treasury
    
    * test treasury

---
+++ contracts/mocks/TestTreasury.sol
@@ -0,0 +1,17 @@
+import "../features/Treasury.sol";
+
+pragma solidity ^0.8.3;
+
+// This contract is designed to hold the erc20 and eth reserves of the dao
+// and will likely control a large amount of funds. It is designed to be
+// flexible, secure and simple
+contract TestTreasury is Treasury {
+    uint256 public dummy;
+
+    constructor(address _governance) Treasury(_governance) {}
+
+    // function to test call forwarding
+    function updateDummy(uint256 _newDummy) public {
+        dummy = _newDummy;
+    }
+}
