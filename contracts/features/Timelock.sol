3d751c959b42573c78ccd0bccbc80424bf6d9a90
Sarah Mackey <sarahmackey42@gmail.com>
2021-08-19 17:29:06 -0700

    Timelock contract (#13)
    * initial commit
    
    * Add authorizable library
    
    * Add in initial testing
    
    * syntax errors
    
    * syntax fun
    
    * More unit tests
    
    * Add increase function, change execution parameters
    
    * address comments, add some testing
    
    * updated modifiers
    
    * debugging tests
    
    * Add governance test
    
    * Add comments
    
    * Address comments from pairing session
    
    * Incorrect but potentially down the right track
    
    * real life working unit test :)
    
    * More unit tests!
    
    * Update Timelock.sol
    
    * Uncomment require on setWaitTime
    
    * Add GSC to constructor, change encoding
    
    * debugging setWaitTime
    
    * Add requested unit tests
    
    * Add comments, restore state after execute

--- contracts/features/Timelock.sol
+++ contracts/features/Timelock.sol
@@ -1,45 +1,101 @@
-// This contract is currently a scaffold, meaning it is unimplemented and
-// just designed to show the shape of future code. The naming and other
-// conventions are 'soft' and more of suggestions that the implementer
-// has full ability to change. Changes which break interface compatibility though
-// should be double checked with the rest of the team.
-
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.3;
+pragma solidity ^0.8.0;
+
+import "../libraries/Authorizable.sol";
 
 // Allows a call to be executed after a waiting period, also allows a call to
 // be canceled within a waiting period.
 
-contract Timelock {
-    // Checks that the caller is the governance contract
+contract Timelock is Authorizable {
+    // Amount of time for the waiting period
+    uint256 public waitTime;
+    // the governance address
+    address public governance;
+    // Mapping of call hashes to block timestamps
+    mapping(bytes32 => uint256) public callTimestamps;
+    // Mapping from a call hash to its status of once allowed time increase
+    mapping(bytes32 => bool) public timeIncreases;
+
+    /// @notice Constructs this contract and sets state variables
+    /// @param _waitTime amount of time for the waiting period
+    /// @param _governance governance
+    /// @param _gsc governance steering comity contract.
+    constructor(
+        uint256 _waitTime,
+        address _governance,
+        address _gsc
+    ) Authorizable() {
+        _authorize(_gsc);
+        waitTime = _waitTime;
+        setOwner(_governance);
+        governance = _governance;
+    }
+
+    /// @dev Modifier checks if the msg.sender is the governance contract
     modifier onlyGovernance() {
+        require(msg.sender == governance, "contract must be governance");
         _;
     }
 
-    // Checks that the caller is making an external call from
-    // this address
-    modifier onlySelf() {
-        _;
-    }
-
+    /// @notice Stores at the callHash the current block timestamp
+    /// @param callHash The hash to map the timestamp to
     function registerCall(bytes32 callHash) external onlyGovernance {
-        // stores at the callHash the current block timestamp
+        callTimestamps[callHash] = block.timestamp;
     }
 
+    /// @notice Removes stored callHash data
+    /// @param callHash Which entry of the mapping to remove
     function stopCall(bytes32 callHash) external onlyGovernance {
-        // removes stored callHash data
+        delete callTimestamps[callHash];
     }
 
-    function execute(
-        bytes32 callHash,
-        address target,
-        bytes calldata callData
-    ) external {
-        // loads the stored callHash data and checks enough time has passed
-        // Hashes the provided data and checks it matches the callHash
-        // executes call
+    /// @notice Execute the call if past the waiting period
+    /// @param targets List of target addresses the timelock contract will interact with
+    /// @param calldatas Execution calldata for each target
+    function execute(address[] memory targets, bytes[] calldata calldatas)
+        public
+    {
+        // hash provided data to access the mapping
+        bytes32 callHash = keccak256(abi.encode(targets, calldatas));
+        // call defaults to zero and cannot be executed before it is registered
+        require(callTimestamps[callHash] != 0, "call has not been initialized");
+        // call cannot be executed before the waiting period has passed
+        require(
+            callTimestamps[callHash] + waitTime < block.timestamp,
+            "not enough time has passed"
+        );
+        // execute a package of low level calls
+        for (uint256 i = 0; i < targets.length; i++) {
+            (bool success, bytes memory returnData) =
+                targets[i].call(calldatas[i]);
+            // revert if a single call fails
+            require(success == true, "call reverted");
+        }
+        // restore state after successful execution
+        delete callTimestamps[callHash];
     }
 
-    // Allow a call from this contract to reset the wait time storage variable
-    function setWaitTime(uint256 waitTime) external onlySelf {}
+    /// @notice Allow a call from this contract to reset the wait time storage variable
+    /// @param _waitTime New wait time to set to
+    function setWaitTime(uint256 _waitTime) public {
+        require(msg.sender == address(this), "contract must be self");
+        waitTime = _waitTime;
+    }
+
+    /// @notice Allow an increase in wait time for a given call
+    /// can only be executed once for each call
+    /// @param timeValue Amount of time to increase by
+    /// @param callHash The mapping entry to increase time
+    function increaseTime(uint256 timeValue, bytes32 callHash)
+        external
+        onlyAuthorized
+    {
+        require(
+            timeIncreases[callHash] == false,
+            "value can only be changed once"
+        );
+        callTimestamps[callHash] += timeValue;
+        // set mapping to indicate call has been changed
+        timeIncreases[callHash] = true;
+    }
 }
---
+++ test/timelockTest.ts
@@ -0,0 +1,218 @@
+import "module-alias/register";
+
+import { expect } from "chai";
+import { BytesLike } from "ethers";
+import { ethers, waffle } from "hardhat";
+import { createSnapshot, restoreSnapshot } from "./helpers/snapshots";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { Timelock } from "typechain";
+import timelockData from "../artifacts/contracts/features/Timelock.sol/Timelock.json";
+
+const { provider } = waffle;
+
+export async function createCallHash(calldata: BytesLike[], targets: string[]) {
+  const toBeHashed = ethers.utils.defaultAbiCoder.encode(
+    ["address[]", "bytes[]"],
+    [targets, calldata]
+  );
+  return ethers.utils.keccak256(toBeHashed);
+}
+
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+describe("Timelock", () => {
+  let timelock: Timelock;
+
+  let signers: SignerWithAddress[];
+
+  async function getBlock() {
+    return (await ethers.provider.getBlock("latest")).number;
+  }
+
+  before(async () => {
+    await createSnapshot(provider);
+    signers = await ethers.getSigners();
+
+    const deployer = await ethers.getContractFactory("Timelock", signers[0]);
+    timelock = await deployer.deploy(0, signers[0].address, signers[1].address);
+  });
+
+  after(async () => {
+    await restoreSnapshot(provider);
+  });
+
+  describe("execute", () => {
+    beforeEach(async () => {
+      await createSnapshot(provider);
+    });
+    afterEach(async () => {
+      await restoreSnapshot(provider);
+    });
+
+    it("fails to execute prematurely", async () => {
+      const newWaitTime = 100000000000000;
+      const tInterface = new ethers.utils.Interface(timelockData.abi);
+      const calldata = tInterface.encodeFunctionData("setWaitTime", [
+        newWaitTime,
+      ]);
+
+      const callHash = await createCallHash([calldata], [timelock.address]);
+      await timelock.connect(signers[0]).registerCall(callHash);
+
+      // execute once to update wait time through the execute function
+      await timelock
+        .connect(signers[0])
+        .execute([timelock.address], [calldata]);
+      // execute again to check against the new wait time
+      await timelock.connect(signers[0]).registerCall(callHash);
+      const tx = timelock
+        .connect(signers[0])
+        .execute([timelock.address], [calldata]);
+
+      await expect(tx).to.be.revertedWith("not enough time has passed");
+    });
+
+    it("fails if call not registered", async () => {
+      const calldata = ["0x12345678ffffffff", "0x12345678ffffffff"];
+      const callHash = await createCallHash(calldata, [timelock.address]);
+
+      const call = await timelock.callTimestamps(callHash);
+      expect(call).to.be.eq(0);
+      const tx = timelock
+        .connect(signers[0])
+        .execute([timelock.address], calldata);
+      await expect(tx).to.be.revertedWith("call has not been initialized");
+    });
+
+    it("fail if executed twice", async () => {
+      const newWaitTime = 10000;
+      const tInterface = new ethers.utils.Interface(timelockData.abi);
+      const calldata = tInterface.encodeFunctionData("setWaitTime", [
+        newWaitTime,
+      ]);
+
+      const callHash = await createCallHash([calldata], [timelock.address]);
+
+      await timelock.connect(signers[0]).registerCall(callHash);
+      await delay(1000);
+      await timelock
+        .connect(signers[0])
+        .execute([timelock.address], [calldata]);
+      const tx = timelock
+        .connect(signers[0])
+        .execute([timelock.address], [calldata]);
+      await expect(tx).to.be.revertedWith("call has not been initialized");
+    });
+
+    it("successful execution", async () => {
+      const newWaitTime = 10000;
+      const tInterface = new ethers.utils.Interface(timelockData.abi);
+      const calldata = tInterface.encodeFunctionData("setWaitTime", [
+        newWaitTime,
+      ]);
+
+      const callHash = await createCallHash([calldata], [timelock.address]);
+
+      await timelock.connect(signers[0]).registerCall(callHash);
+      await delay(1000);
+      await timelock
+        .connect(signers[0])
+        .execute([timelock.address], [calldata]);
+
+      const w = await timelock.waitTime();
+      expect(w).to.be.eq(newWaitTime);
+
+      // Check that state has been restored after successful execute
+      const call = await timelock.callTimestamps(callHash);
+      expect(call).to.be.eq(0);
+    });
+  });
+
+  describe("increase time", () => {
+    beforeEach(async () => {
+      await createSnapshot(provider);
+    });
+    afterEach(async () => {
+      await restoreSnapshot(provider);
+    });
+
+    it("fails if not authorized", async () => {
+      const calldata = ["0x12345678ffffffff", "0x12345678ffffffff"];
+      const callHash = await createCallHash(calldata, [timelock.address]);
+
+      const tx = timelock.connect(signers[0]).increaseTime(1000, callHash);
+      await expect(tx).to.be.revertedWith("Sender not Authorized");
+    });
+
+    it("fails if attempted more than once", async () => {
+      const calldata = ["0x12345678ffffffff", "0x12345678ffffffff"];
+      const callHash = await createCallHash(calldata, [timelock.address]);
+
+      await timelock.connect(signers[1]).increaseTime(1234, callHash);
+      const tx = timelock.connect(signers[1]).increaseTime(5678, callHash);
+      await expect(tx).to.be.revertedWith("value can only be changed once");
+    });
+
+    it("successful time increase", async () => {
+      const calldata = ["0x12345678ffffffff", "0x12345678ffffffff"];
+      const callHash = await createCallHash(calldata, [timelock.address]);
+
+      await timelock.connect(signers[1]).increaseTime(1234, callHash);
+      const call = await timelock.callTimestamps(callHash);
+      expect(call).to.be.eq(1234);
+    });
+  });
+
+  describe("register call", () => {
+    beforeEach(async () => {
+      await createSnapshot(provider);
+    });
+    afterEach(async () => {
+      await restoreSnapshot(provider);
+    });
+
+    it("fails if not governance", async () => {
+      const calldata = ["0x12345678ffffffff", "0x12345678ffffffff"];
+      const callHash = await createCallHash(calldata, [timelock.address]);
+
+      const tx = timelock.connect(signers[1]).registerCall(callHash);
+      await expect(tx).to.be.revertedWith("contract must be governance");
+    });
+
+    it("successful call register", async () => {
+      const calldata = ["0x12345678ffffffff", "0x12345678ffffffff"];
+      const callHash = await createCallHash(calldata, [timelock.address]);
+      await timelock.connect(signers[0]).registerCall(callHash);
+      const call = await timelock.callTimestamps(callHash);
+      expect(call).to.not.eq(0);
+    });
+  });
+
+  describe("stop call", () => {
+    beforeEach(async () => {
+      await createSnapshot(provider);
+    });
+    afterEach(async () => {
+      await restoreSnapshot(provider);
+    });
+
+    it("fails if not governance", async () => {
+      const calldata = ["0x12345678ffffffff", "0x12345678ffffffff"];
+      const callHash = await createCallHash(calldata, [timelock.address]);
+
+      const tx = timelock.connect(signers[1]).stopCall(callHash);
+      await expect(tx).to.be.revertedWith("contract must be governance");
+    });
+
+    it("successful call stop", async () => {
+      const calldata = ["0x12345678ffffffff", "0x12345678ffffffff"];
+      const callHash = await createCallHash(calldata, [timelock.address]);
+      await timelock.connect(signers[0]).registerCall(callHash);
+      await timelock.connect(signers[0]).stopCall(callHash);
+      const call = await timelock.callTimestamps(callHash);
+      expect(call).to.be.eq(0);
+    });
+  });
+});
