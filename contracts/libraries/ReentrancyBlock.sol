7b7f3fb8e7cd1699c150fd38b00ebc1b4b0f0a49
Paul Vienhage <paul@0x.org>
2021-09-21 16:13:21 +0100

    lint

--- contracts/libraries/ReentrancyBlock.sol
+++ contracts/libraries/ReentrancyBlock.sol
@@ -3,16 +3,16 @@ pragma solidity ^0.8.3;
 
 contract ReentrancyBlock {
     // A storage slot for the reentrancy flag
-    bool entered;
+    bool private _entered;
     // Will use a state flag to prevent this function from being called back into
     modifier nonReentrant() {
         // Check the state variable before the call is entered
-        require(!entered, "Reentrancy");
+        require(!_entered, "Reentrancy");
         // Store that the function has been entered
-        entered = true;
+        _entered = true;
         // Run the function code
         _;
         // Clear the state
-        entered = false;
+        _entered = false;
     }
 }
