c8d37e2df4b96d94d456a8b12494cfee0fff3981
Sarah Mackey <sarahmackey42@gmail.com>
2022-01-31 12:43:04 -0800

    Add destination for withdrawal

--- contracts/vaults/NonvotingVault.sol
+++ contracts/vaults/NonvotingVault.sol
@@ -3,22 +3,32 @@ pragma solidity ^0.8.3;
 
 import "../libraries/Authorizable.sol";
 import "../interfaces/ILockingVault.sol";
+import "../interfaces/IERC20.sol";
 
 contract NonvotingVault is Authorizable {
     // The locking gov vault
     ILockingVault public lockingVault;
+    IERC20 public immutable token;
 
     /// @notice Constructs this contract
     /// @param _owner The address authorized to withdraw
     /// @param _lockingVault The governance vault which this withdraws from
-    constructor(address _owner, ILockingVault _lockingVault) Authorizable() {
+    constructor(
+        address _owner,
+        IERC20 _token,
+        ILockingVault _lockingVault
+    ) Authorizable() {
         setOwner(_owner);
         lockingVault = _lockingVault;
+        token = _token;
+        _token.approve(address(lockingVault), type(uint256).max);
     }
 
     /// @notice Withdraws from the locking vault
     /// @param amount The amount to withdraw
-    function withdraw(uint256 amount) external {
+    /// @param destination The location to send the withdrawn tokens
+    function withdraw(uint256 amount, address destination) external onlyOwner {
         lockingVault.withdraw(amount);
+        token.transfer(destination, amount);
     }
 }
