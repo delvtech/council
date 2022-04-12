123f1bc0a608adee2c93773ceca90c6925bc69bc
Sarah Mackey <sarahmackey42@gmail.com>
2022-04-12 09:47:19 -0700

    Update BalanceQuery.sol

--- contracts/BalanceQuery.sol
+++ contracts/BalanceQuery.sol
@@ -7,45 +7,38 @@ import "./interfaces/IVotingVault.sol";
 
 contract BalanceQuery is Authorizable {
     // stores approved voting vaults
-    mapping(address => bool) public approvedVaults;
+    IVotingVault[] public vaults;
 
     /// @notice Constructs this contract and stores needed data
     /// @param _owner The contract owner authorized to remove vaults
-    constructor(address _owner) {
-        // authorize the owner address to be able to remove vaults
+    constructor(address _owner, address[] memory votingVaults) {
+        // set the storage array of vaults
+        vaults = new IVotingVault[](votingVaults.length);
+
+        // authorize the owner address to be able to add/remove vaults
         _authorize(_owner);
     }
 
-    function balanceOfVault(
-        address user,
-        bytes calldata extraData,
-        address vault
-    ) external returns (uint256) {
-        // check that vault is verified
-        require(approvedVaults[vault], "unverified vault");
-
-        // query the vault that we put into storage to load the users balance for each vault
-        uint256 votingPower =
-            IVotingVault(vault).queryVotePower(
-                user,
-                block.number - 1,
-                extraData
-            );
+    function balanceOfVaults(address user) external returns (uint256) {
+        uint256 votingPower = 0;
+        // query voting power from each vault
+        for (uint256 i = 0; i < vaults.length; i++) {
+            votingPower =
+                votingPower +
+                vault.queryVotePower(user, block.number - 1, 0);
+        }
 
         // return that balance
         return votingPower;
     }
 
     function addVault(address vault) external onlyAuthorized {
-        require(approvedVaults[vault] == false, "vault already added");
-        approvedVaults[vault] = true;
+        // ?
     }
 
     function removeVault(address vault) external onlyAuthorized {
-        require(
-            approvedVaults[vault] == true,
-            "vault already removed or does not exist"
-        );
-        approvedVaults[vault] = false;
+        for (uint256 i = 0; i < vaults.length; i++) {
+            delete vault[i];
+        }
     }
 }
