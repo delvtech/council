e7229c79c190092de264d8ece92f7bd59936aaed
Sarah Mackey <sarahmackey42@gmail.com>
2022-04-12 12:55:04 -0700

    fix array initialization & update, comments

--- contracts/BalanceQuery.sol
+++ contracts/BalanceQuery.sol
@@ -11,29 +11,43 @@ contract BalanceQuery is Authorizable {
 
     /// @notice Constructs this contract and stores needed data
     /// @param _owner The contract owner authorized to remove vaults
+    /// @param votingVaults An array of the vaults to query balances from
     constructor(address _owner, address[] memory votingVaults) {
-        // set the storage array of vaults
+        // create a new array of voting vaults
         vaults = new IVotingVault[](votingVaults.length);
+        // populate array with each vault passed into constructor
+        for (uint256 i = 0; i < votingVaults.length; i++) {
+            vaults[i] = IVotingVault(votingVaults[i]);
+        }
 
         // authorize the owner address to be able to add/remove vaults
         _authorize(_owner);
     }
 
+    /// @notice Queries and adds together the vault balances for specified user
+    /// @param user The user to query balances for
+    /// @return The total voting power for the user
     function balanceOfVaults(address user) external returns (uint256) {
         uint256 votingPower = 0;
-        // query voting power from each vault
+        // query voting power from each vault and add to total
         for (uint256 i = 0; i < vaults.length; i++) {
             votingPower =
                 votingPower +
-                vault.queryVotePower(user, block.number - 1, 0);
+                vaults[i].queryVotePower(user, block.number - 1, "0x");
         }
-
         // return that balance
         return votingPower;
     }
 
-    function updateVaults(address[] memory vaults) external onlyAuthorized {
-        // set the storage array of vaults
-        vaults = new IVotingVault[](votingVaults.length);
+    /// @notice Updates the storage variable for vaults to query
+    /// @param _vaults An array of the new vaults to store
+    function updateVaults(address[] memory _vaults) external onlyAuthorized {
+        // reset our array in storage
+        vaults = new IVotingVault[](_vaults.length);
+
+        // populate with each vault passed into the method
+        for (uint256 i = 0; i < _vaults.length; i++) {
+            vaults[i] = IVotingVault(_vaults[i]);
+        }
     }
 }
--- test/balanceQueryTest.ts
+++ test/balanceQueryTest.ts
@@ -1,17 +1,25 @@
 import { expect } from "chai";
 import { ethers, waffle } from "hardhat";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { BigNumber } from "ethers";
 import { createSnapshot, restoreSnapshot } from "./helpers/snapshots";
-import { BalanceQuery, LockingVault, VestingVault, MockERC20 } from "typechain";
+import {
+  BalanceQuery,
+  LockingVault,
+  TestVestingVault,
+  MockERC20,
+} from "typechain";
 
 const { provider } = waffle;
 
 describe("Balance Query", function () {
   let balanceQuery: BalanceQuery;
   let signers: SignerWithAddress[];
-  let vault: LockingVault;
+  let lockingVault: LockingVault;
+  let vestingVault: TestVestingVault;
   let token: MockERC20;
   const one = ethers.utils.parseEther("1");
+  const amount: BigNumber = ethers.utils.parseEther("10");
 
   before(async () => {
     // Create a before snapshot
@@ -24,27 +32,42 @@ describe("Balance Query", function () {
       signers[0]
     );
     token = await erc20Deployer.deploy("Ele", "test ele", signers[0].address);
+    // set token amount
+    await token.setBalance(signers[0].address, amount);
 
-    // deploy the contract
-    const vaultDeployer = await ethers.getContractFactory(
+    // deploy the locking vault
+    const lockingVaultDeployer = await ethers.getContractFactory(
       "LockingVault",
       signers[0]
     );
-    vault = await vaultDeployer.deploy(token.address, 199350);
+    lockingVault = await lockingVaultDeployer.deploy(token.address, 199350);
+
+    // deploy the vesting vault
+    const vestingVaultDeployer = await ethers.getContractFactory(
+      "TestVestingVault",
+      signers[0]
+    );
+    vestingVault = await vestingVaultDeployer.deploy(token.address, 199350);
+    // set approval and deposit some tokens
+    await token.connect(signers[0]).approve(vestingVault.address, amount);
+    await vestingVault.connect(signers[0]).deposit(amount);
 
     // deploy the contract
     const deployer = await ethers.getContractFactory(
       "BalanceQuery",
       signers[0]
     );
-    balanceQuery = await deployer.deploy(signers[0].address);
+    balanceQuery = await deployer.deploy(signers[0].address, [
+      lockingVault.address,
+      vestingVault.address,
+    ]);
 
     // Give users some balance and set their allowance
     for (const signer of signers) {
       await token.setBalance(signer.address, ethers.utils.parseEther("100000"));
       await token.setAllowance(
         signer.address,
-        vault.address,
+        lockingVault.address,
         ethers.constants.MaxUint256
       );
     }
@@ -53,38 +76,38 @@ describe("Balance Query", function () {
   after(async () => {
     await restoreSnapshot(provider);
   });
-  describe("Add vault", async () => {
-    it("fails to add already added vault", async () => {
-      const mockVestingVaultAddress =
-        "0x814C447a9F58A2b823504Fe2775bA48c843925B6";
-      // successfully add vault once
-      await balanceQuery.addVault(mockVestingVaultAddress);
-      // second add should fail with the same address
-      const tx = balanceQuery.addVault(mockVestingVaultAddress);
-      await expect(tx).to.be.revertedWith("vault already added");
-    });
-  });
-  describe("Remove vault", async () => {
-    it("fails to remove nonexistent vault", async () => {
-      const mockLockingVaultAddress =
-        "0x5a0b54d5dc17e0aadc383d2db43b0a0d3e029c4c";
-      const tx = balanceQuery.removeVault(mockLockingVaultAddress);
-      await expect(tx).to.be.revertedWith(
-        "vault already removed or does not exist"
-      );
-    });
-    it("fails to remove vault that has already been removed", async () => {
-      const mockVestingVaultAddress =
-        "0x814C447a9F58A2b823504Fe2775bA48c843925B6";
-      // successfully removes vault once
-      await balanceQuery.removeVault(mockVestingVaultAddress);
-      // second removal should fail
-      const tx = balanceQuery.removeVault(mockVestingVaultAddress);
-      await expect(tx).to.be.revertedWith(
-        "vault already removed or does not exist"
-      );
-    });
-  });
+  // describe("Add vault", async () => {
+  //   it("fails to add already added vault", async () => {
+  //     const mockVestingVaultAddress =
+  //       "0x814C447a9F58A2b823504Fe2775bA48c843925B6";
+  //     // successfully add vault once
+  //     await balanceQuery.addVault(mockVestingVaultAddress);
+  //     // second add should fail with the same address
+  //     const tx = balanceQuery.addVault(mockVestingVaultAddress);
+  //     await expect(tx).to.be.revertedWith("vault already added");
+  //   });
+  // });
+  // describe("Remove vault", async () => {
+  //   it("fails to remove nonexistent vault", async () => {
+  //     const mockLockingVaultAddress =
+  //       "0x5a0b54d5dc17e0aadc383d2db43b0a0d3e029c4c";
+  //     const tx = balanceQuery.removeVault(mockLockingVaultAddress);
+  //     await expect(tx).to.be.revertedWith(
+  //       "vault already removed or does not exist"
+  //     );
+  //   });
+  //   it("fails to remove vault that has already been removed", async () => {
+  //     const mockVestingVaultAddress =
+  //       "0x814C447a9F58A2b823504Fe2775bA48c843925B6";
+  //     // successfully removes vault once
+  //     await balanceQuery.removeVault(mockVestingVaultAddress);
+  //     // second removal should fail
+  //     const tx = balanceQuery.removeVault(mockVestingVaultAddress);
+  //     await expect(tx).to.be.revertedWith(
+  //       "vault already removed or does not exist"
+  //     );
+  //   });
+  // });
   //   describe("Check balance of vault", async () => {
   //     // Before each we snapshot
   //     beforeEach(async () => {
