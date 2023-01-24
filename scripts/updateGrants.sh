bc9b18e4cb57d5313225394f99ff6c563537ee73
Matthew Brown <mattfdbrown@gmail.com>
2023-01-24 07:27:35 -0800

    update bash scripts

--- scripts/updateGrants.sh
+++ scripts/updateGrants.sh
@@ -3,9 +3,18 @@
 
 NETWORK=localhost
 
+# deploy the upgrade contract
 npx hardhat run scripts/deployUpgradeContract.ts --no-compile --network $NETWORK
+
+# creating and executing the proposal
 npx hardhat run scripts/previewUpgradeProposal.ts --no-compile --network $NETWORK
-npx hardhat run scripts/setQuorum.ts --no-compile --network $NETWORK
+
+#remove this for actual executuion
+# npx hardhat run scripts/setQuorum.ts --no-compile --network $NETWORK
+
 npx hardhat run scripts/createUpgradeProposal.ts --no-compile --network $NETWORK
-npx hardhat run scripts/jumpForward.ts --no-compile --network $NETWORK
+
+#remove this for actual execution
+# npx hardhat run scripts/jumpForward.ts --no-compile --network $NETWORK
+
 npx hardhat run scripts/executeUpgradeProposal.ts --no-compile --network $NETWORK
---
+++ scripts/updateTimestamps.sh
@@ -0,0 +1,6 @@
+
+# Script to update grants with new values
+
+NETWORK=localhost
+
+npx hardhat run src/updateTimeStampsTest.ts --no-compile --network $NETWORK
