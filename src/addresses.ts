95388c09c1601eebc6e28818a5233fac973a20cc
Matthew Brown <mattfdbrown@gmail.com>
2023-01-27 07:54:33 -0800

    update the unfrozen vesting vault address

--- src/addresses.ts
+++ src/addresses.ts
@@ -13,7 +13,7 @@ const addressesJson = {
     treasury: "0x82eF450FB7f06E3294F2f19ed1713b255Af0f541",
     vestingVault: "0x6De73946eab234F1EE61256F10067D713aF0e37A",
     frozenVestingVaultAddress: "0x716D4e863536aC862AD34bC4eCaCBa07d8831bEA",
-    unfrozenVestingVaultAddress: "0x1D13fF25b10C9a6741DFdce229073bed652197c7",
+    unfrozenVestingVaultAddress: "0x01cf58e264d7578D4C67022c58A24CbC4C4a304E",
   },
   chainId: 1,
 };
