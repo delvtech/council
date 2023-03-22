b9cd3ccf218aef194f50547bce024da9cc683e2e
Matthew Brown <mattfdbrown@gmail.com>
2023-03-21 19:05:03 -0700

    get 2nd wallet address

--- scripts/helpers/privateKeyFromMnemonic.ts
+++ scripts/helpers/privateKeyFromMnemonic.ts
@@ -3,8 +3,9 @@ import { ethers } from "ethers";
 async function main() {
   const mnemonic =
     "test test test test test test test test test test test test";
-  const mnemonicWallet = ethers.Wallet.fromMnemonic(mnemonic);
-  console.log("mnemonicWallet", mnemonicWallet.privateKey);
+  const wallet = ethers.utils.HDNode.fromMnemonic(mnemonic);
+  const wallet2 = wallet.derivePath("m/44'/60'/0'/0/1");
+  console.log("wallet2 private key: ", wallet2.privateKey);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
